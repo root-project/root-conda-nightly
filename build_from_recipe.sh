8af72c5d06899649e25e953dae5894c09818b478
Enrico Guiraud <enrico.guiraud@cern.ch>
2020-06-13 12:31:26 +0200

    Remove unused helper scripts

--- build_from_recipe.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
-
-apt-get update && apt-get install --yes binutils
-conda install --yes --quiet conda-build make
-git clone http://github.com/conda-forge/root-feedstock
-conda build -c conda-forge root-feedstock/recipe
