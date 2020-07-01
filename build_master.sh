02b439be7f45ac8bee27ddb8fa2d3a639429a040
Enrico Guiraud <enrico.guiraud@cern.ch>
2020-07-01 15:20:16 +0200

    Minor improvements to build_master.sh

--- build_master.sh
+++ build_master.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+set -x
 
-export ROOT_CONDA_BUILD_NUMBER=20200630
+set +ux
+echo -n "activating conda..."
+conda activate
+echo "done"
+conda update --yes --all --quiet
+conda install --yes --quiet -c conda-build git
+set -ux
+
+export ROOT_CONDA_BUILD_NUMBER="$(date +%Y%M%d)"
 export ROOT_CONDA_GIT_URL=https://github.com/root-project/root.git
 export ROOT_CONDA_GIT_REV=master
-export ROOT_CONDA_USE_CCACHE=1
+export ROOT_CONDA_USE_CCACHE=0  # disabled at least for now
 export ROOT_CONDA_RUN_GTESTS=1
 
 git clone https://github.com/chrisburr/clangdev-feedstock.git -b root-nightlies
