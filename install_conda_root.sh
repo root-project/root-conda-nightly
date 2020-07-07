01fe560e4121c399944bd90515437ea1f9fc217f
Enrico Guiraud <enrico.guiraud@cern.ch>
2020-07-07 22:38:49 +0200

    Parameterize install_conda_root.sh over channel and ROOT version

--- install_conda_root.sh
+++ install_conda_root.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Install ROOT as a conda package
-# TODO: parameterize script over ROOT version/conda package label
+# If the CUSTOM_CONDA_CHANNEL env variable is set, that channel will take priority over conda-forge
+# when looking for root and its dependencies.
+# If the ROOT_VERSION env variable is set it will be passed to the conda installation command.
 
 #--- setup environment ---#
 set -euo pipefail
@@ -12,5 +14,6 @@ echo -n "activating conda..."
 conda activate
 echo "done"
 set -ux
+
 conda update --yes --all --quiet
-conda install --yes --quiet -c conda-forge root
+conda install --yes --quiet ${CUSTOM_CONDA_CHANNEL:+-c ${CUSTOM_CONDA_CHANNEL}} -c conda-forge root${ROOT_VERSION:+=${ROOT_VERSION}}
