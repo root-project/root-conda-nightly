6c5c5261ff17d765e43e4b45d7f6a07b89c68172
Chris Burr <christopher.burr@cern.ch>
2021-03-15 15:54:30 +0100

    Debugging

--- build_master.sh
+++ build_master.sh
@@ -5,11 +5,14 @@ set -x
 
 # ROOT tends to use 120% of cores for builds
 export CPU_COUNT=$(expr 12 \* $(nproc) / 10 )
-export ROOT_CONDA_BUILD_NUMBER="$(date +%Y%M%d)"
-export ROOT_CONDA_GIT_URL=https://github.com/root-project/root.git
-export ROOT_CONDA_GIT_REV=master
-export ROOT_CONDA_USE_CCACHE=0  # disabled at least for now
-export ROOT_CONDA_RUN_GTESTS=1
+
+CONDA_FORGE_DOCKER_RUN_ARGS="--rm"
+CONDA_FORGE_DOCKER_RUN_ARGS+=" -e ROOT_CONDA_BUILD_NUMBER=$(date +%Y%M%d)"
+CONDA_FORGE_DOCKER_RUN_ARGS+=" export ROOT_CONDA_GIT_URL=https://github.com/root-project/root.git"
+CONDA_FORGE_DOCKER_RUN_ARGS+=" export ROOT_CONDA_GIT_REV=master"
+CONDA_FORGE_DOCKER_RUN_ARGS+=" export ROOT_CONDA_USE_CCACHE=0"
+CONDA_FORGE_DOCKER_RUN_ARGS+=" export ROOT_CONDA_RUN_GTESTS=1"
+export CONDA_FORGE_DOCKER_RUN_ARGS
 
 rm -rf clangdev-feedstock && git clone https://github.com/chrisburr/clangdev-feedstock.git -b root-nightlies-2
 rm -rf cling-feedstock && git clone https://github.com/chrisburr/cling-feedstock.git -b root-nightlies-2
