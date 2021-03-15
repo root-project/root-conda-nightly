953519994c28b64b9343dc48f43b54b1b527a8d2
Chris Burr <christopher.burr@cern.ch>
2021-03-15 16:31:08 +0100

    Debugging

--- build_master.sh
+++ build_master.sh
@@ -11,10 +11,10 @@ export CI=1
 
 CONDA_FORGE_DOCKER_RUN_ARGS="--rm"
 CONDA_FORGE_DOCKER_RUN_ARGS+=" -e ROOT_CONDA_BUILD_NUMBER=$(date +%Y%M%d)"
-CONDA_FORGE_DOCKER_RUN_ARGS+=" export ROOT_CONDA_GIT_URL=https://github.com/root-project/root.git"
-CONDA_FORGE_DOCKER_RUN_ARGS+=" export ROOT_CONDA_GIT_REV=master"
-CONDA_FORGE_DOCKER_RUN_ARGS+=" export ROOT_CONDA_USE_CCACHE=0"
-CONDA_FORGE_DOCKER_RUN_ARGS+=" export ROOT_CONDA_RUN_GTESTS=1"
+CONDA_FORGE_DOCKER_RUN_ARGS+=" -e ROOT_CONDA_GIT_URL=https://github.com/root-project/root.git"
+CONDA_FORGE_DOCKER_RUN_ARGS+=" -e ROOT_CONDA_GIT_REV=master"
+CONDA_FORGE_DOCKER_RUN_ARGS+=" -e ROOT_CONDA_USE_CCACHE=0"
+CONDA_FORGE_DOCKER_RUN_ARGS+=" -e ROOT_CONDA_RUN_GTESTS=1"
 export CONDA_FORGE_DOCKER_RUN_ARGS
 
 rm -rf clangdev-feedstock && git clone https://github.com/chrisburr/clangdev-feedstock.git -b root-nightlies-2
